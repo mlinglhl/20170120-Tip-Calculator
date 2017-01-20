08efcd1e87a83d3ecee06b1fd09cede97588641d
mlinglhl <minhung.ling@gmail.com>
2017-01-20 11:44:06 -0500

    Initial Commit

---
+++ 20170120 Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20170120 Tip Calculator
+//
+//  Created by Minhung Ling on 2017-01-20.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
